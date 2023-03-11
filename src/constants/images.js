af1ad6885fdb5b086ec90d3b580e371ee3657644
kshitija17 <kshitija17@live.com>
2023-03-11 15:17:08 +0000

    cosmetics-app

---
+++ src/constants/images.js
@@ -0,0 +1,47 @@
+import bg from '../assets/bg.png';
+import chef from '../assets/chef-section.jpg';
+import G from '../assets/G.png';
+import gallery01 from '../assets/gallary-1.jpg';
+import gallery02 from '../assets/gallary-2.jpg';
+import gallery03 from '../assets/gallary-3.jpg';
+import gallery04 from '../assets/gallary-4.jpg';
+import knife from '../assets/brush.png';
+import logo from '../assets/logo.png';
+import menu from '../assets/ingredients.jpg';
+import overlaybg from '../assets/overlaybg.png';
+import spoon from '../assets/spoon.svg';
+import welcome from '../assets/header.jpg';
+import findus from '../assets/find-us.jpg';
+import laurels from '../assets/cosmetics-laurels.jpg';
+import award01 from '../assets/award01.png';
+import award02 from '../assets/award02.png';
+import award03 from '../assets/award03.png';
+import award05 from '../assets/award05.png';
+import sign from '../assets/sign.png';
+import quote from '../assets/quote.png';
+import gericht from '../assets/logo.png';
+
+export default {
+  bg,
+  chef,
+  G,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  knife,
+  logo,
+  menu,
+  overlaybg,
+  spoon,
+  welcome,
+  findus,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  sign,
+  quote,
+  gericht,
+};
